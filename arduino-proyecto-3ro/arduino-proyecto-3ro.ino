e1a03d24a14b1a31ed7b0a3f7e9e145e0b701d15
Nico Clari <clarinicolas7@gmail.com>
2026-04-22 15:14:09 -0300

    Update arduino-proyecto-3ro.ino
    Creamos un cuenta regresiva para poder leer los datos de los sensores...
    Arreglamos la lectura de los sensores de humedad.
    Aumentamos los sensores DHT internos y externos a 2.
    
    Q.E.P.D pepe :(((

--- arduino-proyecto-3ro/arduino-proyecto-3ro.ino
+++ arduino-proyecto-3ro/arduino-proyecto-3ro.ino
@@ -2,12 +2,18 @@
 #include <DHT.h>
 
 #define DHT_PIN 2
-#define DHT_PIN2 1
+#define DHT_PIN2 12
 #define DHTTYPE DHT22
 #define INDUCTIVE_PIN 3
 #define SENSOR1 6
 #define SENSOR2 7
 #define SENSOR3 8
+#define LED_R 9 // ERROR!
+#define LED_G 10 // ORGANICO
+#define LED_B 11 // INORGANICO
+
+unsigned long lastReadTime = 0;
+const long interval = 60000;
 
 DHT dht(DHT_PIN, DHTTYPE);
 DHT dht2(DHT_PIN2, DHT11);
@@ -22,29 +28,42 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(digitalRead(SENSOR1));
+  //Serial.println(digitalRead(SENSOR1));
   if (digitalRead(SENSOR1) == 0) {
     Serial.println("Inductivo");
     int inductivo = digitalRead(INDUCTIVE_PIN);
     if (inductivo) {
     Serial.println("metal");
+    onled (LED_B);
     }
     else{
     Serial.println("no metal");
+    onled (LED_G);
 
     }
   }
 if (digitalRead(SENSOR2) == 0) {
-    Serial.println("dht:");
-    Serial.println("dht2:");
 
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    float humidity2 = dht2.readHumidity();
-    float temperature2 = dht2.readTemperature();
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  float humidity2 = dht2.readHumidity();
+  float temperature2 = dht2.readTemperature();
+
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("Error leyendo DHT22");
-  } else {
+    onled(LED_R);
+  } 
+  else {    
+
+    Serial.println("Leyendo en 1 minuto");
+
+    for (int i = 60; i > 0; i -= 5) {
+      Serial.print("Aguarde ");
+      Serial.print(i);
+      Serial.println(" segundos");
+      delay(5000); // espera real de 5 segundos
+    }
+
     Serial.print("Humedad: ");
     Serial.print(humidity);
     Serial.print(" %  |  Temp: ");
@@ -56,10 +75,38 @@ if (digitalRead(SENSOR2) == 0) {
     Serial.print(" %  |  Temp Interna: ");
     Serial.print(temperature2);
     Serial.println(" C");
-  }
+
+    if (humidity2 - humidity >= 15) {
+      Serial.println("Organico"); // comillas dobles
+      onled(LED_G);
     }
+    Serial.println("Leyendo en 1 minuto");
+
+    for (int i = 60; i > 0; i -= 5) {
+      Serial.print("Aguarde ");
+      Serial.print(i);
+      Serial.println(" segundos");
+      delay(5000); // espera real de 5 segundos
+    }
+    Serial.println("Leyendo en 2 minuto");
+
+    for (int i = 120; i > 0; i -= 10) {
+      Serial.print("Aguarde ");
+      Serial.print(i);
+      Serial.println(" segundos");
+      delay(10000); // espera real de 5 segundos
+    }
+  }
+}
 if (digitalRead(SENSOR3) == 0) {
   Serial.println('SENSOR3');
   }
-delay(50000000);
+delay(500);
+}
+
+void onled(int led){
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
 }
